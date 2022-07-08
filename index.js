461b99ccc5cf2e78876590854ec6651105ccb690
Vaibhav Mishra <mishravaibhav950@gmail.com>
2022-07-08 19:14:57 +0530

    Added Navbar and Footer

---
+++ index.js
@@ -0,0 +1,93 @@
+
+let mainDiv = document.querySelector(".box");
+let allDivs = document.querySelectorAll(".box>div.animatable");
+let scrollerDiv = document.querySelector('#scrollDiv');
+let headerImg = document.querySelector('nav>.logo>img');
+
+
+scrollerDiv.addEventListener('click', () => {
+
+    if (window.scrollY < 400) {
+        window.scrollBy(0, 400)
+    }
+});
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(CSSPlugin);
+
+gsap.to(headerImg, {
+    filter: 'none',
+    duration: 1,
+    scaleX:1,
+    scaleY:1,
+    ease: "power4.in"
+});
+
+gsap.to(mainDiv, {
+    left: 0,
+    opacity: 1,
+    duration: 0.75,
+    ease: "power4.in"
+});
+
+allDivs.forEach(element => {
+    gsap.to(element, {
+        scrollTrigger: {
+            trigger: element,
+            toggleActions: "restart none none reset",
+            start: "top 95%"
+        },
+        opacity: 1,
+        top: 0,
+        duration: 0.75,
+        ease: "power4.in"
+    });
+});
+gsap.to(".skillList .skillItem:nth-child(1) div:nth-child(2) div", {
+    scrollTrigger: {
+        trigger: ".skillItem:nth-child(1)",
+        toggleActions: "restart none none reset",
+        start: "top 95%",
+    },
+    width: "95%"
+});
+gsap.to(".skillList .skillItem:nth-child(2) div:nth-child(2) div", {
+    scrollTrigger: {
+        trigger: ".skillItem:nth-child(2)",
+        toggleActions: "restart none none reset",
+        start: "top 95%",
+    },
+    width: "85%"
+});
+gsap.to(".skillList .skillItem:nth-child(3) div:nth-child(2) div", {
+    scrollTrigger: {
+        trigger: ".skillItem:nth-child(3)",
+        toggleActions: "restart none none reset",
+        start: "top 95%",
+    },
+    width: "75%"
+});
+gsap.to(".skillList .skillItem:nth-child(4) div:nth-child(2) div", {
+    scrollTrigger: {
+        trigger: ".skillItem:nth-child(4)",
+        toggleActions: "restart none none reset",
+        start: "top 95%",
+    },
+    width: "80%"
+});
+gsap.to(".skillList .skillItem:nth-child(5) div:nth-child(2) div", {
+    scrollTrigger: {
+        trigger: ".skillItem:nth-child(5)",
+        toggleActions: "restart none none reset",
+        start: "top 95%",
+    },
+    width: "90%"
+});
+gsap.to(".skillList .skillItem:nth-child(6) div:nth-child(2) div", {
+    scrollTrigger: {
+        trigger: ".skillItem:nth-child(6)",
+        toggleActions: "restart none none reset",
+        start: "top 95%"
+    },
+    width: "93%"
+});
